cbf6a04ce00ae550f435c44994c28127646c9570
Paul Smith <paulcsmith0218@gmail.com>
2018-04-16 19:25:21 -0400

    Setting up spec_helper

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,17 @@
 require "spec"
 require "../src/authentic"
+
+LuckyRecord::Repo.configure do
+  if ENV["DATABASE_URL"]?
+    settings.url = ENV["DATABASE_URL"]
+  else
+    settings.url = LuckyRecord::PostgresURL.build(
+      database: "authentic_test",
+      hostname: "localhost"
+    )
+  end
+end
+
+Spec.before_each do
+  LuckyRecord::Repo.truncate
+end
