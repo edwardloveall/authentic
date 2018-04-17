f716220abc034f3bc21b19b7d1bc0f1c7607435a
Paul Smith <paulcsmith0218@gmail.com>
2018-04-16 20:53:46 -0400

    Testing it out

--- spec/authentic_spec.cr
+++ spec/authentic_spec.cr
@@ -1,19 +1,31 @@
 require "./spec_helper"
 
-include ContextHelper
-
 class Test::Action < Lucky::Action
   get "/" { text "Doesn't matter" }
 end
 
 describe Authentic do
   it "remembers the requested path if it is a GET " do
-    context = build_context(path: "/direct_here_after_sign_in")
-    action = Test::Action.new(context, {} of String => String)
+    context = ContextHelper.new(path: "/direct_here_after_sign_in").build
+    action = Test::Action.new(context, empty_params)
     action.session[:return_to].should be_nil
 
     Authentic.remember_requested_path(action)
 
     action.session[:return_to].should eq "/direct_here_after_sign_in"
   end
+
+  it "does not remember the requested path if it isn't a GET " do
+    context = ContextHelper.new(method: "POST").build
+    action = Test::Action.new(context, empty_params)
+    action.session[:return_to].should be_nil
+
+    Authentic.remember_requested_path(action)
+
+    action.session[:return_to].should be_nil
+  end
+end
+
+private def empty_params
+  {} of String => String
 end
--- spec/support/context_helper.cr
+++ spec/support/context_helper.cr
@@ -1,35 +1,15 @@
-module ContextHelper
-  private def build_request(method = "GET", body = "", content_type = "")
-    headers = HTTP::Headers.new
-    headers.add("Content-Type", content_type)
-    HTTP::Request.new(method, "/", body: body, headers: headers)
+class ContextHelper
+  private getter path, method
+
+  def initialize(
+    @path : String = "/",
+    @method : String = "GET"
+  )
   end
 
-  private def build_context(path = "/", request = nil) : HTTP::Server::Context
-    build_context_with_io(IO::Memory.new, path: path, request: request)
-  end
-
-  private def build_context(method : String) : HTTP::Server::Context
-    build_context_with_io(
-      IO::Memory.new,
-      path: "/",
-      request: build_request(method)
-    )
-  end
-
-  private def build_context_with_io(io : IO, path = "/", request = nil) : HTTP::Server::Context
-    request = request || HTTP::Request.new("GET", path)
-    response = HTTP::Server::Response.new(io)
-    HTTP::Server::Context.new request, response
-  end
-
-  private def build_context_with_flash(flash : String)
-    build_context.tap do |context|
-      context.session[Lucky::Flash::Handler::PARAM_KEY] = flash
-    end
-  end
-
-  private def params
-    {} of String => String
+  def build : HTTP::Server::Context
+    request = HTTP::Request.new(method, path)
+    response = HTTP::Server::Response.new(IO::Memory.new)
+    HTTP::Server::Context.new(request, response)
   end
 end
