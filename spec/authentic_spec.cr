95bddcc6aebe155d93e6a868eb962fd50b1dd93a
Paul Smith <paulcsmith0218@gmail.com>
2018-04-17 09:33:58 -0400

    Add empty specs to fill in later

--- spec/authentic_spec.cr
+++ spec/authentic_spec.cr
@@ -54,7 +54,7 @@ describe Authentic do
     response.context.response.headers["Location"].should eq Fallback::Action.path
   end
 
-  it "redirects to fallback RouteHelper if return to is not set" do
+  it "redirects to fallback RouteHelper if return_to is not set" do
     context = ContextHelper.new.build
     action = Test::Action.new(context, empty_params)
 
@@ -64,6 +64,24 @@ describe Authentic do
     )
 
     response.context.response.headers["Location"].should eq Fallback::Action.path
+  end
+
+  it "can check whether the given password is correct or not" do
+  end
+
+  it "can save an encrypted password to a LuckyRecord::Field" do
+  end
+
+  it "can create a hashed password" do
+  end
+
+  it "can generate a password reset token" do
+  end
+
+  it "can check if the password reset token is correct" do
+  end
+
+  it "MAYBE: It can send a password reset email" do
   end
 end
 
